faf0d0e0944dc3fcf0d2c119611f112eee949a20
woong1116 <tkdldh77@naver.com>
2026-07-23 09:43:13 +0900

    7.23

---
+++ moneylog/src/main/java/com/likelion/moneylog/MoneylogApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.moneylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneylogApplication.class, args);
+	}
+
+}
---
+++ moneylog/src/test/java/com/likelion/moneylog/MoneylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.moneylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
